7b2dbad32bbdb87a9be4f2038e8fb6bc1c1246a6
AbdulrhmanElshaer31 <abdelrhmanelshaer31gmail.com>
2026-03-10 22:29:21 +0200

    Reset BTN Added!

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const counter_field = document.getElementById("counter-field");
 const increase_btn = document.getElementById("btn");
 const decrease_btn = document.getElementById("btn2");
-
+const reset_btn = document.getElementById("btn3");
 increase_btn.addEventListener("click", () => {
   let count = counter_field.textContent;
   count++;
@@ -16,3 +16,10 @@ decrease_btn.addEventListener("click", () => {
   counter_field.textContent = count;
   console.log(count);
 });
+
+
+reset_btn.addEventListener("click", () => {
+  
+  counter_field.textContent = 0;
+});
+
