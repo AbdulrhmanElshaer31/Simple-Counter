18c53580cbe331ac95af343c0b83d5edeec0a93b
AbdulrhmanElshaer31 <abdelrhmanelshaer31gmail.com>
2026-03-10 22:24:09 +0200

    decrease btn added!

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 //define constans
 const counter_field = document.getElementById("counter-field");
 const increase_btn = document.getElementById("btn");
+const decrease_btn = document.getElementById("btn2");
 
 increase_btn.addEventListener("click", () => {
-    let count = counter_field.textContent;
-    count++;
-    counter_field.textContent = count;
-    console.log(count);
-    
-})
+  let count = counter_field.textContent;
+  count++;
+  counter_field.textContent = count;
+  console.log(count);
+});
+
+decrease_btn.addEventListener("click", () => {
+  let count = counter_field.textContent;
+  count--;
+  counter_field.textContent = count;
+  console.log(count);
+});
